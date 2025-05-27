459a8baa5f5f9d65e861736745d20b71007dbef6
Xiaoqian Hu <xihu9333@uni.sydney.edu.au>
2025-05-27 13:38:02 +1000

    2

--- agents/user_config.py
+++ agents/user_config.py
@@ -1,9 +1,7 @@
-from agents.rag_agent import SimpleRAGAgent
-from agents.random_agent import RandomAgent
-from agents.vanilla_llama_vision_agent import LlamaVisionModel
-from agents.single_source_agent import SingleSourceAgent
+from agents.enhanced_single_source_agent import EnhancedSingleSourceAgent
 
-# UserAgent = RandomAgent
-# UserAgent = SimpleRAGAgent
-# UserAgent = LlamaVisionModel
-UserAgent = SingleSourceAgent
+
+
+UserAgent = EnhancedSingleSourceAgent
+
+
